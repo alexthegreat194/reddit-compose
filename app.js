888d68eb7fac388096fbe07087bec5dd72648d2c
alexthegreat194 <alexander.gentry@aol.com>
2022-04-19 15:32:39 -0700

    setup tailwind and changed file extention

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const { engine } = require('express-handlebars');
 
 const app = express();
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', engine({
+    extname: 'hbs',
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'hbs');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./views/**/*.hbs", "./views/layouts/**/*.hbs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
