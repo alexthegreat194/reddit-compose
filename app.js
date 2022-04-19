afbbf8164c7da47b3c7e2f36d68b5366fefb4222
alexthegreat194 <alexander.gentry@aol.com>
2022-04-19 15:08:28 -0700

    added handlebars

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
 
 const app = express();
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('home');
 });
 
-
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
-});
+app.listen(3000);
