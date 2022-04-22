7a3ce596f9d12105769790a33efda51c32d427ba
alexthegreat194 <alexander.gentry@aol.com>
2022-04-21 20:30:00 -0700

    can create users

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 
 const {PrismaClient} = require('@prisma/client')
+const bcrypt = require('bcrypt')
+
 const prisma = new PrismaClient()
+
+const generateHash = async (password) => {
+    const passwordHash = await bcrypt.hash(password, 10)
+    .catch(err => console.log(err))
+    return passwordHash;
+}
 
 module.exports = (app) => {
     
@@ -8,8 +16,26 @@ module.exports = (app) => {
         res.render('signup.hbs')
     });
 
+    app.post('/signup', async (req, res) => {
+        
+        let password = req.body.password;
+        let passwordHash = await generateHash(password);
+        
+        prisma.user.create({
+            data: {
+                username: req.body.username,
+                password: passwordHash
+            }
+        })
+        .then(() => console.log('created user'))
+
+        res.redirect('/');
+    });
+
     app.get('/login', (req, res) => {
         res.render('login')
     })
+
+    
 
 }
