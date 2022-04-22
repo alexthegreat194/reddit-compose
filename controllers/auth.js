e7b561d61f0e1be5aca4de05c87ac94b3bd52c23
alexthegreat194 <alexander.gentry@aol.com>
2022-04-21 18:13:46 -0700

    added auth forms

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,12 @@ const prisma = new PrismaClient()
 
 module.exports = (app) => {
     
-    
+    app.get('/signup', (req, res) => {
+        res.render('signup.hbs')
+    });
+
+    app.get('/login', (req, res) => {
+        res.render('login')
+    })
 
 }
